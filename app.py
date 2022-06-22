d4bb767dcefb2e3521c695ad51ca758838a086e9
Omar Arnous <omar99.arnous@gmail.com>
2022-06-22 13:32:46 +0300

    First Draft of the ai system

---
+++ app.py
@@ -0,0 +1,13 @@
+from email import message
+import json
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/face')
+def facerecognition():
+    return jsonify({message: 'omar face verified'})
+
+@app.route('/voice')
+def voicerecognition():
+    return jsonify({message: 'omar voice verified'})    
