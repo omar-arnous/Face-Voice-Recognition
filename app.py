8f886f26cb112664aa8e68ec9fc0d1f0ed033540
Omar Arnous <omar99.arnous@gmail.com>
2022-06-24 05:37:30 +0300

    removed saving images

--- app.py
+++ app.py
@@ -11,17 +11,15 @@ app = Flask(__name__)
 @app.route('/face', methods=['POST'])
 def facerecognition():
     jsonify({"message": "face recognition started"})
-    response = request.get_data()
     picture_1 = request.files['picture_1']
     picture_2 = request.files['picture_2']
     # print(picture_1)
     # print(picture_2)
-    pic_1 = werkzeug.utils.secure_filename(picture_1.filename)
-    pic_2 = werkzeug.utils.secure_filename(picture_2.filename)
-    picture_1.save("static/img/" + pic_1)
-    picture_1.save("static/img/" + pic_2)
-    result = FaceRecognition(picture_1, picture_1) 
-    name = 'omar'
+    # pic_1 = werkzeug.utils.secure_filename(picture_1.filename)
+    # pic_2 = werkzeug.utils.secure_filename(picture_2.filename)
+    # picture_1.save("images" + pic_1)
+    # picture_1.save("images" + pic_2)
+    result = FaceRecognition(picture_1, picture_2) 
     if result:
         return jsonify({'result': 1})
     else:
